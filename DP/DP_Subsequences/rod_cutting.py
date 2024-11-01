e86fc02429c9660f8677e4868fb5dbcea48e23ef
Jayasurya <jaya.svn@iitgn.ac.in>
2024-11-02 02:16:44 +0530

    DP Subsequences Done

--- DP/DP_Subsequences/rod_cutting.py
+++ DP/DP_Subsequences/rod_cutting.py
@@ -1,23 +1,17 @@
 from typing import List,Optional
-import sys
+from collections import deque, defaultdict
+import sys, math, heapq
+
 class Solution:
     def cutRod(self, price, n):
-        dp = [[0]*(n+1) for _ in range(n)]
-
-        for j in range(1,n+1):
-            dp[0][j] = price[0]*j
-
-        for i in range(1,n):
-            for rodlen in range(0,n+1):
-                dp[i][rodlen] = dp[i-1][rodlen]
-                if(i+1<=rodlen):
-                    dp[i][rodlen] = max(dp[i][rodlen],dp[i][rodlen-(i+1)]+price[i])
-        return dp[n-1][n] 
-
-                
+        dp = [0]*(n+1)
+        for i in range(1,n+1):
+            for l in range(1,i+1):
+                dp[i] = max(dp[i],dp[i-l]+price[l-1])
+        return dp[n]
 
 # time complexity: O(n^2)
-# space complexity: O(n^2)
+# space complexity: O(n)
 if __name__ == "__main__":
     for _ in range(int(input().strip())):
         n = int(input().strip())
