7624198acb53090e031f32c5170856122852aca1
Jayasurya <jaya.svn@iitgn.ac.in>
2025-01-03 14:55:33 +0530

    21/12

--- Daily_Problems/2024/December/q21.py
+++ Daily_Problems/2024/December/q21.py
@@ -3,12 +3,28 @@ from collections import deque, defaultdict
 import sys, math, heapq, bisect
 
 class Solution:
-    def func(self, arr:List,n:int):
-        pass
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        graph = [[] for _ in range(n)]
+        for a,b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        self.ans = 0
+        def dfs(u,parent):
+            sum = 0
+            for v in graph[u]:
+                if(v!=parent):sum+=dfs(v,u)
+            
+            sum+=values[u]
+            sum%=k
+            if(sum==0):self.ans+=1
+            return sum
+            
+        dfs(0,-1)
+        return self.ans
 
-
-# time complexity: O()
-# space complexity: O()
+# time complexity: O(m+n)
+# space complexity: O(m+n)
 if __name__ == "__main__":
     for _ in range(int(input().strip())):
         n = int(input().strip())
