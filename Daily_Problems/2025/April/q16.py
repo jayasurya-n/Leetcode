c27002dd2f4a103b76f889f1a54f85fbd2c659eb
Jayasurya <jaya.svn@iitgn.ac.in>
2025-04-16 13:24:39 +0530

    16/4

--- Daily_Problems/2025/April/q16.py
+++ Daily_Problems/2025/April/q16.py
@@ -30,6 +30,6 @@ class Solution:
 # space complexity: O(n)
 if __name__ == "__main__":
     for _ in range(ii()):
-        n = ii()
-        arr = lii()
-        print(Solution().func(arr,n))
+        nums = lii()
+        k = ii()
+        print(Solution().countGood(nums,k))
