bd164cb10da01f4f4c0e1ec24156dc5fa72f474b
Jayasurya <jaya.svn@iitgn.ac.in>
2025-04-16 13:22:50 +0530

    15/4

---
+++ Daily_Problems/2025/April/q16.py
@@ -0,0 +1,35 @@
+from typing import List,Optional
+from collections import deque, defaultdict
+import sys, math, heapq, bisect
+
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        hash = defaultdict(int)
+        pairs = ans = 0
+        
+        i = j = 0
+        while j<n:
+            hash[nums[j]]+=1
+            pairs+=hash[nums[j]]-1
+            while i<j and pairs>=k:
+                hash[nums[i]]-=1
+                pairs-=hash[nums[i]]
+                ans+=(n-j)
+                i+=1
+            j+=1
+        
+        return ans
+
+# time complexity: O(n)
+# space complexity: O(n)
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
