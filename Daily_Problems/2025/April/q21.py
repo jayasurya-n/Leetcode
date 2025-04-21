5b85525f7ad93caa89eecd9f4129bddcde4bd279
Jayasurya <jaya.svn@iitgn.ac.in>
2025-04-21 13:48:39 +0530

    21/4

---
+++ Daily_Problems/2025/April/q21.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
