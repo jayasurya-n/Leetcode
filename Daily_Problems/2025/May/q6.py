36f6cf176b65436e64dcd912f97cdefea3758503
Jayasurya <jaya.svn@iitgn.ac.in>
2025-05-06 12:08:45 +0530

    6/5

---
+++ Daily_Problems/2025/May/q6.py
@@ -0,0 +1,20 @@
+from typing import List,Optional
+from collections import deque, defaultdict
+import sys, math, heapq, bisect
+
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        return [nums[nums[i]] for i in range(len(nums))]
+
+# time complexity: O(n)
+# space complexity: O(1)
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
