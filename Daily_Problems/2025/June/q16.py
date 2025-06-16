145cd8abc72ec0f89fb66b469e74bc9134859f18
Jayasurya <jaya.svn@iitgn.ac.in>
2025-06-16 12:39:54 +0530

    16/6

---
+++ Daily_Problems/2025/June/q16.py
@@ -0,0 +1,25 @@
+from typing import List,Optional
+from collections import deque, defaultdict
+import sys, math, heapq, bisect
+
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        mini = 10**10
+        ans = -1
+        for i in range(len(nums)):
+            if(nums[i]>mini):ans = max(ans,nums[i]-mini)
+            else:mini = min(mini,nums[i])
+        return ans
+
+# time complexity: O(n)
+# space complexity: O(1)
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
