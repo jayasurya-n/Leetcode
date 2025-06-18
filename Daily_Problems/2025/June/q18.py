c2f3ee1f905cb6af37fd5476b90d3beb3297ef4f
Jayasurya <jaya.svn@iitgn.ac.in>
2025-06-18 11:41:18 +0530

    18/6

---
+++ Daily_Problems/2025/June/q18.py
@@ -0,0 +1,26 @@
+from typing import List,Optional
+from collections import deque, defaultdict
+import sys, math, heapq, bisect
+
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for i in range(0,len(nums),3):
+            if(nums[i+2]-nums[i]<=k):
+                ans.append([nums[i],nums[i+1],nums[i+2]])
+            else:return []
+        return ans
+
+# time complexity: O(nlogn)
+# space complexity: O(1)
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
