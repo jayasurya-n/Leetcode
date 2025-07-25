3aafcfb1982f948b5ed680c76b1257db4d93d6f4
Jayasurya <jaya.svn@iitgn.ac.in>
2025-07-25 12:17:54 +0530

    25/7

---
+++ Daily_Problems/2025/July/q25.py
@@ -0,0 +1,24 @@
+from typing import List,Optional
+from collections import deque, defaultdict
+import sys, math, heapq, bisect
+
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        maxi = max(nums)
+        if(maxi<=0):return maxi
+
+        nums = set([num for num in nums if num>0])
+        return sum(nums)
+
+# time complexity: O(n)
+# space complexity: O(n)
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
