597603f09d498e32ab2f9cb6566c27190ecd643c
Jayasurya <jaya.svn@iitgn.ac.in>
2025-08-09 00:08:39 +0530

    8/8

--- Daily_Problems/2025/August/q8.py
+++ Daily_Problems/2025/August/q8.py
@@ -9,29 +9,50 @@ lii = lambda: list(map(int,input().strip().split()))
 
 class Solution:
     def soupServings(self, n: int) -> float:
-        m = math.ceil(n/25)
-        if m>200:return 1.0
+        # m = math.ceil(n/25)
+        # if m>200:return 1.0
 
-        dp = [[0.0]*(m+1) for _ in range(m+1)]
-        dp[0][0] = 0.5
-        for i in range(1,m+1):
-            dp[0][i] = 1.0
-            dp[i][0] = 0.0
+        # dp = [[0.0]*(m+1) for _ in range(m+1)]
+        # dp[0][0] = 0.5
+        # for i in range(1,m+1):
+        #     dp[0][i] = 1.0
+        #     dp[i][0] = 0.0
 
-        for i in range(1,m+1):
-            for j in range(1,m+1):
-                dp[i][j] = (
-                    dp[max(i-4, 0)][j] +
-                    dp[max(i-3, 0)][max(j-1, 0)] +
-                    dp[max(i-2, 0)][max(j-2, 0)] +
-                    dp[max(i-1, 0)][max(j-3, 0)])/4.0
+        # for i in range(1,m+1):
+        #     for j in range(1,m+1):
+        #         dp[i][j] = (
+        #             dp[max(i-4, 0)][j] +
+        #             dp[max(i-3, 0)][max(j-1, 0)] +
+        #             dp[max(i-2, 0)][max(j-2, 0)] +
+        #             dp[max(i-1, 0)][max(j-3, 0)])/4.0
 
-            if dp[i][i] > 1-1e-5:return 1.0
+        #     if dp[i][i] > 1-1e-5:return 1.0
         
-        return dp[m][m]
+        # return dp[m][m]
 
-# time complexity: O(m^2)
-# space complexity: O(m^2)
+        m = math.ceil(n/25)
+
+        def solve(i,j):
+            if(i<=0 and j<=0):return 0.5
+            if(i<=0 and j>0):return 1.0
+            if(i>0 and j<=0):return 0.0
+
+            if((i,j) in dp):return dp[(i,j)]
+            dp[(i,j)] = (
+                solve(i-4,j) +
+                solve(i-3,j-1) +
+                solve(i-2,j-2) +
+                solve(i-1,j-3))/4.0
+            return dp[(i,j)]
+
+        dp = defaultdict(float)
+        for i in range(1,m+1):
+            if(solve(i,i)>1-1e-5):return 1.0
+        
+        return dp[(m,m)]
+
+# time complexity: O(m^2),O(m^2)
+# space complexity: O(m^2),O(m^2)
 if __name__ == "__main__":
     for _ in range(1):
         for n in range(0,200):
