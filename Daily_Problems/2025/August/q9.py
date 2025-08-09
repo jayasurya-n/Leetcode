28cabae26113e0876710eb8ac96bd2589577d805
Jayasurya <jaya.svn@iitgn.ac.in>
2025-08-09 18:59:53 +0530

    8/8

---
+++ Daily_Problems/2025/August/q9.py
@@ -0,0 +1,20 @@
+from typing import List,Optional
+from collections import deque, defaultdict
+import sys, math, heapq, bisect
+
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n&(n-1)==0 and n!=0
+
+# time complexity: O(1)
+# space complexity: O(1)
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
