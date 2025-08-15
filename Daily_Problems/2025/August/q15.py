7645504b46242a0052d03dbad2ef77a54491ffea
Jayasurya <jaya.svn@iitgn.ac.in>
2025-08-15 12:33:46 +0530

    15/8

---
+++ Daily_Problems/2025/August/q15.py
@@ -0,0 +1,24 @@
+from typing import List,Optional
+from collections import deque, defaultdict
+import sys, math, heapq, bisect
+
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if(n<=0):return False
+        while n!=1:
+            if(n%4!=0):return False
+            n//=4
+        return True
+
+# time complexity: O(logn)
+# space complexity: O(1)
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
