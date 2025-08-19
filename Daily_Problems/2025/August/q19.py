37c95792657d36a1a379236a9a87999f4619f532
Jayasurya <jaya.svn@iitgn.ac.in>
2025-08-19 13:07:45 +0530

    19/8

---
+++ Daily_Problems/2025/August/q19.py
@@ -0,0 +1,26 @@
+from typing import List,Optional
+from collections import deque, defaultdict
+import sys, math, heapq, bisect
+
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        zeros = ans = 0
+        for num in nums:
+            if(num==0):
+                zeros+=1
+                ans+=zeros
+            else:zeros = 0
+        return ans
+
+# time complexity: O(n)
+# space complexity: O(1)
+if __name__ == "__main__":
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
